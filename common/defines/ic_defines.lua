9540c371614e0c54a5e147282975252b99702749
True Leifee!!! <102321360+neeLeifee@users.noreply.github.com>
2025-01-18 01:48:35 +0300

    added world tension

--- common/defines/ic_defines.lua
+++ common/defines/ic_defines.lua
@@ -5,6 +5,7 @@ NDefines.NGame.COMBAT_LOG_MAX_MONTHS = 6
 -- Diplomacy
 NDefines.NDiplomacy.BASE_SEND_ATTACHE_COST = 0
 NDefines.NDiplomacy.BASE_SEND_ATTACHE_CP_COST = 0
+NDefines.NDiplomacy.WAR_SUPPORT_TENSION_IMPACT = 0.5
 
 -- Country
 NDefines.NCountry.BASE_RESEARCH_SLOTS = 3
