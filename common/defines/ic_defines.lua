09bda901f3f1a8cc65f951fd9bd13f5bf43bf6af
True Leifee!!! <102321360+neeLeifee@users.noreply.github.com>
2026-07-04 01:37:15 +0300

    sov theorists done

--- common/defines/ic_defines.lua
+++ common/defines/ic_defines.lua
@@ -858,7 +858,7 @@ NDefines.NProject.SCIENTIST_SKILL_LEVEL_THRESHOLDS = {0,0,0,0,0}
 NDefines.NProject.SCIENTIST_SKILL_LEVEL_SPEED_MODIFIER = {0,0,0,0,0,0}
 
 --Doctrines
-NDefines.NDoctrines.DEFAULT_REWARD_MASTERY = 50.0                         -- How much mastery is required for unlocking a doctrine reward, if no override is set
+NDefines.NDoctrines.DEFAULT_REWARD_MASTERY = 100.0                         -- How much mastery is required for unlocking a doctrine reward, if no override is set
 NDefines.NDoctrines.BASE_MASTERY_GAIN_TARGET_MANPOWER = 100000.0           -- Beyond this amount of manpower contributing to mastery, mastery gain will start having diminishing returns (see doctrines documentation)
 NDefines.NDoctrines.TRAINING_MASTERY_GAIN_FACTOR = 0                     -- How much training contributes to doctrine mastery relative to combat/missions
 NDefines.NDoctrines.MAX_MONTHLY_MASTERY_GAIN = 15                       -- Monthly mastery gain will not exceed this value
